297caacbbb58508210373b42d514bcee301515c6
nkbaifang@gmail.com <nkbaifang@gmail.com>
2018-01-11 11:04:39 +0800

    Fix

--- scripts/app/main3d.js
+++ scripts/app/main3d.js
@@ -1,7 +1,7 @@
 define(require => {
     'use strict';
-
-	const GLOBAL = require('global');
+    
+    const GLOBAL = require('global');
     const THREE = require('three');
     const Vector = require('vector');
     const Object = require('object');
@@ -9,12 +9,12 @@ define(require => {
     const _create_scene = () => {
         let _scene = new THREE.Scene();
 
-	    let _light = new THREE.AmbientLight(0xffffff, 0.3);
-	    _scene.add(_light);
+        let _light = new THREE.AmbientLight(0xffffff, 0.3);
+        _scene.add(_light);
 
-	    _light = new THREE.DirectionalLight(0xffffff, 1,0);
-	    _light.position.set(100, 0, 100);
-	    _scene.add(_light);
+        _light = new THREE.DirectionalLight(0xffffff, 1,0);
+        _light.position.set(100, 0, 100);
+        _scene.add(_light);
 
         return _scene;
     };
@@ -35,8 +35,8 @@ define(require => {
         _renderer.gammaInput = true;
         _renderer.gammaOutput = true;
 
-	    _renderer.shadowMap.enabled = true;
-	    _renderer.shadowMap.type = THREE.PCFShadowMap;
+        _renderer.shadowMap.enabled = true;
+        _renderer.shadowMap.type = THREE.PCFShadowMap;
         
         return _renderer;
     };
@@ -58,12 +58,12 @@ define(require => {
     };
 
     const _create_objects = (a = []) => {
-    	return a.reduce((s, c) => [...s, _create_object(c)], []);
+        return a.reduce((s, c) => [...s, _create_object(c)], []);
     };
 
-	const _next_position = (o, interval) => {
-		return o.p.add(o.v.scale(interval));
-	};
+    const _next_position = (o, interval) => {
+        return o.p.add(o.v.scale(interval));
+    };
     
     const _module = {
         
@@ -74,27 +74,27 @@ define(require => {
         _config: {},
         
         _objs: [],
-
-	    _gravity_to(one, ...others) {
-		    let self = this;
-		    const G = self._config.G;
-		    return others.reduce((v, o) => {
-			    let _d = o.p.add(one.p.minus());
-			    let _dl = _d.length;
-			    let _f = G * one.mass * o.mass / (_dl * _dl);
-			    v.append(Vector.of(_f, _d));
-			    return v;
-		    }, Vector.zero(3));
-	    },
-
-	    _add_line(start, end, material) {
-		    let self = this;
-		    let geometry = new THREE.Geometry();
-		    geometry.vertices.push(start);
-		    geometry.vertices.push(end);
-		    let line = new THREE.Line(geometry, material);
-		    self._scene.add(line);
-	    },
+        
+        _gravity_to(one, ...others) {
+            let self = this;
+            const G = self._config.G;
+            return others.reduce((v, o) => {
+                let _d = o.p.add(one.p.minus());
+                let _dl = _d.length;
+                let _f = G * one.mass * o.mass / (_dl * _dl);
+                v.append(Vector.of(_f, _d));
+                return v;
+            }, Vector.zero(3));
+        },
+        
+        _add_line(start, end, material) {
+            let self = this;
+            let geometry = new THREE.Geometry();
+            geometry.vertices.push(start);
+            geometry.vertices.push(end);
+            let line = new THREE.Line(geometry, material);
+            self._scene.add(line);
+        },
 
         _step() {
             let self = this;
@@ -102,113 +102,113 @@ define(require => {
 
             self._objs.forEach(sphere => {
 
-            	let o = sphere.object;
+                let o = sphere.object;
 
-            	if ( _config.box ) {
+                if ( _config.box ) {
 
-            		let [xmin, xmax] = _config.box.x;
-		            let [ymin, ymax] = _config.box.y;
-		            let [zmin, zmax] = _config.box.z;
+                    let [xmin, xmax] = _config.box.x;
+                    let [ymin, ymax] = _config.box.y;
+                    let [zmin, zmax] = _config.box.z;
 
-		            let [px, py, pz] = o.p.values;
-		            let [vx, vy, vz] = o.v.values;
+                    let [px, py, pz] = o.p.values;
+                    let [vx, vy, vz] = o.v.values;
 
-		            let _hit = false;
+                    let _hit = false;
 
-		            if ( (px >= xmax - o.radius && vx > 0)
-			            || (px <= xmin + o.radius && vx < 0) ) {
-			            o.v.apply([
-				            -1, 0, 0,
-				            0, 1, 0,
-				            0, 0, 1
-			            ]);
-			            _hit = true;
-		            }
+                    if ( (px >= xmax - o.radius && vx > 0)
+                        || (px <= xmin + o.radius && vx < 0) ) {
+                        o.v.apply([
+                            -1, 0, 0,
+                            0, 1, 0,
+                            0, 0, 1
+                        ]);
+                        _hit = true;
+                    }
 
-		            if ( (py >= ymax - o.radius && vy > 0)
-			            || (py <= ymin + o.radius && vy < 0) ) {
-			            o.v.apply([
-				            1, 0, 0,
-				            0, -1, 0,
-				            0, 0, 1
-			            ]);
-			            _hit = true;
-		            }
+                    if ( (py >= ymax - o.radius && vy > 0)
+                        || (py <= ymin + o.radius && vy < 0) ) {
+                        o.v.apply([
+                            1, 0, 0,
+                            0, -1, 0,
+                            0, 0, 1
+                        ]);
+                        _hit = true;
+                    }
 
-		            if ( (pz >= zmax - o.radius && vz > 0)
-			            || (pz <= zmin + o.radius && vz < 0) ) {
-			            o.v.apply([
-				            1, 0, 0,
-				            0, 1, 0,
-				            0, 0, -1
-			            ]);
-			            _hit = true;
-		            }
+                    if ( (pz >= zmax - o.radius && vz > 0)
+                        || (pz <= zmin + o.radius && vz < 0) ) {
+                        o.v.apply([
+                            1, 0, 0,
+                            0, 1, 0,
+                            0, 0, -1
+                        ]);
+                        _hit = true;
+                    }
 
-		            if ( _hit && !_config.elastic ) {
-		            	o.v.apply([
-		            		0.9, 0, 0,
-				            0, 0.9, 0,
-				            0, 0, 0.9
-			            ]);
-		            }
-	            }
-	            
-				let _others = self._objs.reduce((a, s) => s === sphere ? [...a] : [...a, s.object], []);
+                    if ( _hit && !_config.elastic ) {
+                        o.v.apply([
+                            0.9, 0, 0,
+                            0, 0.9, 0,
+                            0, 0, 0.9
+                        ]);
+                    }
+                }
+                
+                let _others = self._objs.reduce((a, s) => s === sphere ? [...a] : [...a, s.object], []);
 
-	            if ( _config.gravity ) {
-		            let _f = self._gravity_to(o, ..._others);
-		            o.a = _f.scale(1.0 / o.mass);
-	            }
+                if ( _config.gravity ) {
+                    let _f = self._gravity_to(o, ..._others);
+                    o.a = _f.scale(1.0 / o.mass);
+                }
 
-	            _others.forEach(b => {
+                _others.forEach(b => {
 
-		            let _dp = o.p.add(b.p.minus());
-		            let _dpl = _dp.length;
-		            if ( _dpl < o.radius + b.radius ) {
+                    let _dp = o.p.add(b.p.minus());
+                    let _dpl = _dp.length;
+                    if ( _dpl < o.radius + b.radius ) {
 
-			            let _np1 = _next_position(o, _config.interval);
-			            let _np2 = _next_position(b, _config.interval);
-						
-			            if ( _np1.add(_np2.minus()).length < _dpl ) {
+                        let _np1 = _next_position(o, _config.interval);
+                        let _np2 = _next_position(b, _config.interval);
+                        
+                        if ( _np1.add(_np2.minus()).length < _dpl ) {
 
-				            let _sd = _dpl * _dpl;
-				            let _dv = o.v.add(b.v.minus());   // o.v - b.v
+                            let _sd = _dpl * _dpl;
+                            let _dv = o.v.add(b.v.minus());   // o.v - b.v
 
-				            let _a = _dp.dot(_dv);
+                            let _a = _dp.dot(_dv);
 
-				            let _m = 2.0 / (o.mass + b.mass);
+                            let _m = 2.0 / (o.mass + b.mass);
 
-				            o.v.append(_dp.scale(-_m * b.mass * _a / _sd));
-				            b.v.append(_dp.minus().scale(-_m * o.mass * _a / _sd));
-			            }
-		            }
+                            o.v.append(_dp.scale(-_m * b.mass * _a / _sd));
+                            b.v.append(_dp.minus().scale(-_m * o.mass * _a / _sd));
+                        }
+                    }
 
-	            });
+                });
             });
 
-	        self._objs.forEach(sphere => {
-		        let o = sphere.object;
-		        o.move(_config.interval);
-	        });
+            self._objs.forEach(sphere => {
+                let o = sphere.object;
+                o.move(_config.interval);
+            });
         },
         
         _draw() {
             let self = this;
-	        self._objs.forEach(sphere => {
-	        	let o = sphere.object;
-		        sphere.position.set(...o.p.values);
-	        });
+            self._objs.forEach(sphere => {
+                let o = sphere.object;
+                sphere.position.set(...o.p.values);
+            });
 
-	        self._renderer.render(self._scene, self._camera);
+            self._renderer.render(self._scene, self._camera);
         },
         
         init(config = {
-	        G: 5e+3,
-	        interval: 1,
+            G: 5e+3,
+            interval: 1,
             control: false,
             elastic: true,
-        	gravity: false
+            gravity: false
         }) {
             let self = this;
             self._config = config;
@@ -225,8 +225,8 @@ define(require => {
             self.reset();
             
             if ( config.control ) {
-            	let [_sx, _sy] = [];
-            	
+                let [_sx, _sy] = [];
+                
                 let _mouse_move = (event) => {
                     
                     let _phi = (_sx - event.clientX) * Math.PI / _canvas.width;
@@ -252,38 +252,38 @@ define(require => {
                 };
     
                 let _mouse_up = (event) => {
-                	event.preventDefault();
-                	
+                    event.preventDefault();
+                    
                     _canvas.removeEventListener('mousemove', _mouse_move, false);
                     _canvas.removeEventListener('mouseup', _mouse_up, false);
                     _canvas.removeEventListener('mouseout', _mouse_up, false);
                 };
-            	
-            	let _mouse_down = (event) => {
-            		event.preventDefault();
-            		
-            		[_sx, _sy] = [event.clientX, event.clientY];
-            		
-            		_canvas.addEventListener('mousemove', _mouse_move, false);
+                
+                let _mouse_down = (event) => {
+                    event.preventDefault();
+                    
+                    [_sx, _sy] = [event.clientX, event.clientY];
+                    
+                    _canvas.addEventListener('mousemove', _mouse_move, false);
                     _canvas.addEventListener('mouseup', _mouse_up, false);
                     _canvas.addEventListener('mouseout', _mouse_up, false);
-	            };
-            	
-            	let _mouse_wheel = (event) => {
-            		let _delta = event.wheelDelta / 10;
+                };
+                
+                let _mouse_wheel = (event) => {
+                    let _delta = event.wheelDelta / 10;
                     let _camera = self._camera;
                     let _spherical = new THREE.Spherical().setFromVector3(_camera.position);
                     _spherical.radius -= _delta;
                     if ( _spherical.radius < 0 ) {
-                    	_spherical.radius = 0;
-                    	return;
+                        _spherical.radius = 0;
+                        return;
                     }
                     _camera.position.setFromSpherical(_spherical);
                     _camera.lookAt(0, 0, 0);
                     _camera.updateMatrix();
                     self._renderer.render(self._scene, self._camera);
-	            };
-            	
+                };
+                
                 _canvas.addEventListener('mousedown', _mouse_down, false);
                 _canvas.addEventListener('mousewheel', _mouse_wheel, false);
             }
@@ -297,49 +297,49 @@ define(require => {
 
             self.stop();
             self._theta = 0;
-	        self._camera.position.set(..._config.camera.p);
-	        self._camera.lookAt(0, 0, 0);
+            self._camera.position.set(..._config.camera.p);
+            self._camera.lookAt(0, 0, 0);
 
-	        self._renderer.clear();
-	        self._scene.remove(...self._objs);
-	        self._objs.length = 0;
-	        let _objs = _create_objects(self._config.objs);
-	        self._objs.push(..._objs);
-	        _objs.forEach(c => {
-		        self._scene.add(c);
-	        });
+            self._renderer.clear();
+            self._scene.remove(...self._objs);
+            self._objs.length = 0;
+            let _objs = _create_objects(self._config.objs);
+            self._objs.push(..._objs);
+            _objs.forEach(c => {
+                self._scene.add(c);
+            });
 
-	        if ( _config.box ) {
-		        let [xmin, xmax] = _config.box.x;
-		        let [ymin, ymax] = _config.box.y;
-		        let [zmin, zmax] = _config.box.z;
+            if ( _config.box ) {
+                let [xmin, xmax] = _config.box.x;
+                let [ymin, ymax] = _config.box.y;
+                let [zmin, zmax] = _config.box.z;
 
-		        let material = new THREE.LineBasicMaterial({ color: 0x222222 });
+                let material = new THREE.LineBasicMaterial({ color: 0x222222 });
 
-		        let _v1 = new THREE.Vector3(xmax, ymin, zmax);
-		        let _v2 = new THREE.Vector3(xmax, ymax, zmax);
-		        let _v3 = new THREE.Vector3(xmin, ymax, zmax);
-		        let _v4 = new THREE.Vector3(xmin, ymin, zmax);
-		        let _v5 = new THREE.Vector3(xmax, ymin, zmin);
-		        let _v6 = new THREE.Vector3(xmax, ymax, zmin);
-		        let _v7 = new THREE.Vector3(xmin, ymax, zmin);
-		        let _v8 = new THREE.Vector3(xmin, ymin, zmin);
+                let _v1 = new THREE.Vector3(xmax, ymin, zmax);
+                let _v2 = new THREE.Vector3(xmax, ymax, zmax);
+                let _v3 = new THREE.Vector3(xmin, ymax, zmax);
+                let _v4 = new THREE.Vector3(xmin, ymin, zmax);
+                let _v5 = new THREE.Vector3(xmax, ymin, zmin);
+                let _v6 = new THREE.Vector3(xmax, ymax, zmin);
+                let _v7 = new THREE.Vector3(xmin, ymax, zmin);
+                let _v8 = new THREE.Vector3(xmin, ymin, zmin);
 
-		        self._add_line(_v1, _v2, material);
-		        self._add_line(_v3, _v4, material);
-		        self._add_line(_v5, _v6, material);
-		        self._add_line(_v7, _v8, material);
-		        self._add_line(_v1, _v5, material);
-		        self._add_line(_v2, _v6, material);
-		        self._add_line(_v3, _v7, material);
-		        self._add_line(_v4, _v8, material);
-		        self._add_line(_v1, _v4, material);
-		        self._add_line(_v2, _v3, material);
-		        self._add_line(_v5, _v8, material);
-		        self._add_line(_v7, _v6, material);
-	        }
+                self._add_line(_v1, _v2, material);
+                self._add_line(_v3, _v4, material);
+                self._add_line(_v5, _v6, material);
+                self._add_line(_v7, _v8, material);
+                self._add_line(_v1, _v5, material);
+                self._add_line(_v2, _v6, material);
+                self._add_line(_v3, _v7, material);
+                self._add_line(_v4, _v8, material);
+                self._add_line(_v1, _v4, material);
+                self._add_line(_v2, _v3, material);
+                self._add_line(_v5, _v8, material);
+                self._add_line(_v7, _v6, material);
+            }
 
-	        self._renderer.render(self._scene, self._camera);
+            self._renderer.render(self._scene, self._camera);
         },
         
         start() {
@@ -365,8 +365,8 @@ define(require => {
         stop() {
             let self = this;
             if ( typeof self._aid !== 'undefined' ) {
-	            GLOBAL.cancelAnimationFrame(self._aid);
-	            delete self._aid;
+                GLOBAL.cancelAnimationFrame(self._aid);
+                delete self._aid;
             }
         }
     };
